142226758cae2deecb34cb869b33c278722946c3
alexis <alexis33190@gmail.com>
2023-07-28 11:17:16 +0200

    maj 1

--- quizz/index.js
+++ quizz/index.js
@@ -9,12 +9,31 @@ questionContainer.addEventListener("click", () => {
   questionContainer.classList.toggle("question-click");
 });
 
+//optionnel ci dessous----------
+btn1.addEventListener("click", () => {
+  let answer = prompt("Enter votre nom !");
+  questionContainer.innerHTML += "<h3>Bravo " + answer + "</h3>";
+});
+//------------------------------
+
 btn1.addEventListener("click", () => {
   reponse.classList.add("show-reponse");
   reponse.style.background = "green";
 });
 
+//optionnel ci dessous-----------
+btn2.addEventListener("click", () => {
+  confirm("voulez vous vraiment vous tromper ?");
+});
+//-------------------------------
+
 btn2.addEventListener("click", () => {
   reponse.classList.add("show-reponse");
   reponse.style.background = "red";
 });
+
+// timer, compte a rebours
+setTimeout(() => {
+  questionContainer.style.background = "orange";
+}, 3000);
+//---------------------------
