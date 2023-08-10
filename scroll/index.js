5b03398f6f3b2f5eabb88077d10d35b5d384757d
giraud-alexis <140321581+giraud-alexis@users.noreply.github.com>
2023-08-10 11:38:58 +0200

    maj 2 mode de navbar

--- scroll/index.js
+++ scroll/index.js
@@ -1,11 +1,27 @@
+//evenement au scroll sur une donné précise
+
 const nav = document.querySelector("nav");
+//
+//window.addEventListener("scroll", () => {
+//  if (window.scrollY > 120) {
+//    nav.style.top = 0;
+//  } else {
+//    nav.style.top = "-50px";
+//  }
+//});
+//-----------------------------------
+
+//evenement au scroll sur la derniere donné renseigner
+
+let lastScroll = 0;
 
 window.addEventListener("scroll", () => {
-  if (window.scrollY > 120) {
-    nav.style.top = 0;
+  if (window.scrollY < lastScroll) {
+    navbar.style.top = 0;
   } else {
-    nav.style.top = "-50px";
+    navbar.style.top = "-50px";
   }
+  lastScroll = window.scrollY;
 });
 
 //evenement suivi de souris navbar//
